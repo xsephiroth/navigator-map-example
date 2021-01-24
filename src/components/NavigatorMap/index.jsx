3d4d9fbfe804efbbf06b185398cab10ec91d04fa
eric <xsephiroth@foxmail.com>
2021-01-24 22:16:15 +0800

    RobotIcon accept props

--- src/components/NavigatorMap/index.jsx
+++ src/components/NavigatorMap/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from 'react';
 import { useDrag, useWheelScale } from './hooks';
 
-export const RobotIcon = ({ position = {} }) => {
+export const RobotIcon = ({ position = {}, ...props }) => {
   return (
     <svg
       style={{
@@ -15,6 +15,7 @@ export const RobotIcon = ({ position = {} }) => {
       }}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 22 22"
+      {...props}
     >
       <defs>
         <clipPath>
