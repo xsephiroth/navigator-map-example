f22240dcae80916c4d61022469b882dd8eda8dd5
eric <xsephiroth@foxmail.com>
2021-01-24 21:47:41 +0800

    RobotIcon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import NavigatorMap from './components/NavigatorMap';
+import NavigatorMap, { RobotIcon } from './components/NavigatorMap';
 
 function App() {
   return (
     <div className="App">
-      <NavigatorMap src="https://pic2.zhimg.com/v2-98d09c1c99edda82d74ec70fc82bd7bd_b.jpg"></NavigatorMap>
+      <NavigatorMap src="https://pic2.zhimg.com/v2-98d09c1c99edda82d74ec70fc82bd7bd_b.jpg">
+        <RobotIcon position={{ x: 10, y: 20, angle: 255 }} />
+        <RobotIcon position={{ x: 80, y: 120, angle: 25 }} />
+        <RobotIcon position={{ x: 480, y: 220, angle: 0 }} />
+      </NavigatorMap>
       {/* <NavigatorMap src="https://p1.pstatp.com/large/pgc-image/6599dea9667440b7872dbf49bc55f96f"></NavigatorMap> */}
     </div>
   );
--- src/components/NavigatorMap/index.jsx
+++ src/components/NavigatorMap/index.jsx
@@ -1,6 +1,38 @@
 import { useState, useRef, forwardRef } from 'react';
 import { useDrag, useWheelScale } from './hooks';
 
+export const RobotIcon = ({ position = {} }) => {
+  return (
+    <svg
+      style={{
+        position: 'absolute',
+        left: position.x,
+        top: position.y,
+        transform: `rotate(${position.angle}deg)`,
+        transition: '0.5s',
+        width: '1em',
+        height: '1em',
+      }}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 22 22"
+    >
+      <defs>
+        <clipPath>
+          <path fill="#00f" fillOpacity=".514" d="m-7 1024.36h34v34h-34z" />
+        </clipPath>
+        <clipPath>
+          <path fill="#aade87" fillOpacity=".472" d="m-6 1028.36h32v32h-32z" />
+        </clipPath>
+      </defs>
+      <path
+        d="m345.44 248.29l-194.29 194.28c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744l171.91-171.91-171.91-171.9c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.29 194.28c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373"
+        transform="matrix(.03541-.00013.00013.03541 2.98 3.02)"
+        fill="#4d4d4d"
+      />
+    </svg>
+  );
+};
+
 const MapImg = forwardRef(({ src, onLoad, ...props }, ref) => {
   const handleOnLoad = (e) => {
     const { width, height } = e.target;
@@ -76,6 +108,7 @@ const NavigatorMap = ({ src, children }) => {
         onLoad={onMapImgLoad}
         onDragStart={(e) => e.preventDefault()}
       />
+      {children}
       <button
         style={{ position: 'absolute', bottom: '1em', right: '1em' }}
         onClick={() => {
