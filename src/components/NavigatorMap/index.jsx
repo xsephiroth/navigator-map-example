3a7555f026b92963370f5165d4dea1691e16053e
eric <xsephiroth@foxmail.com>
2021-01-24 17:08:33 +0800

    Calc drag position

--- src/components/NavigatorMap/index.jsx
+++ src/components/NavigatorMap/index.jsx
@@ -1,16 +1,24 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, forwardRef } from 'react';
 
-const MapImg = ({ src, style, onLoad }) => {
+const MapImg = forwardRef(({ src, onLoad, ...props }, ref) => {
   const handleOnLoad = (e) => {
     const { width, height } = e.target;
     onLoad?.({ width, height });
   };
-  return <img src={src} style={style} alt="mapImg" onLoad={handleOnLoad} />;
-};
+  return (
+    <img src={src} alt="mapImg" onLoad={handleOnLoad} ref={ref} {...props} />
+  );
+});
 
 const NavigatorMap = ({ src, children }) => {
   const [scale, setScale] = useState(1);
+  const [mapPosition, setMapPosition] = useState({
+    x: undefined,
+    y: undefined,
+  });
   const containerRef = useRef();
+  const mapImgRef = useRef();
+  const dragBasePosition = useRef({ x: null, y: null });
 
   const onWheel = (e) => {
     const isScaleUp = e.deltaY < 0;
@@ -37,27 +45,89 @@ const NavigatorMap = ({ src, children }) => {
     setScale(Math.min(scaleTo, 1));
   };
 
+  const handleDragStart = (e) => {
+    // 未载入地图图片时不处理
+    const mapImg = mapImgRef.current;
+    if (!mapImg) return;
+
+    const { clientX: x, clientY: y } = e;
+    dragBasePosition.current = {
+      x,
+      y,
+    };
+
+    // 同步当前的地图图片的position
+    setMapPosition({
+      x: mapImg.offsetLeft,
+      y: mapImg.offsetTop,
+    });
+  };
+
+  const handleDragEnd = () => {
+    dragBasePosition.current = {
+      x: null,
+      y: null,
+    };
+  };
+
+  const handleDragMove = (e) => {
+    // 地图需加载完成
+    const mapImg = mapImgRef.current;
+    if (!mapImg) return;
+
+    // 需要基坐标
+    const base = dragBasePosition.current;
+    if (!base.x) return;
+
+    // 计算基于基坐标的移动值
+    const { clientX: x, clientY: y } = e;
+    const offsetX = x - base.x;
+    const offsetY = y - base.y;
+
+    // 更新地图图片position
+    setMapPosition((prev) => ({
+      x: prev.x + offsetX,
+      y: prev.y + offsetY,
+    }));
+
+    // 更新基坐标用于下次计算移动值
+    base.x = x;
+    base.y = y;
+  };
+
   return (
     <div
       style={{
         width: '100%',
         height: '100%',
-        backgroundColor: 'red',
+        backgroundColor: '#7e7e7e',
         overflow: 'hidden',
+        userSelect: 'none',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        position: 'relative',
       }}
       ref={containerRef}
       onWheel={onWheel}
+      onMouseDown={handleDragStart}
+      onMouseUp={handleDragEnd}
+      onMouseMove={handleDragMove}
     >
       <MapImg
         src={src}
+        ref={mapImgRef}
         style={{
           display: 'block',
+          userSelect: 'none',
           transform: `scale(${scale})`,
+          transformOrigin: 'center center',
+          position: 'absolute',
+          top: mapPosition.y,
+          left: mapPosition.x,
         }}
         onLoad={onMapImgLoad}
+        onDragStart={(e) => e.preventDefault()}
       />
     </div>
   );
